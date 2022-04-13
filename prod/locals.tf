63a74b9d7fe28bb2ffb0d027db5a81bee8f00b9c
ifaizan <faizan.sysops@gmail.com>
2022-04-13 12:50:20 +0000

    Added powershell script and changed subnet names

--- prod/locals.tf
+++ prod/locals.tf
@@ -2,7 +2,7 @@ locals {
   vnet_info = {
     "VNET-PROD-SWT-WESTEU-OMS" = {
       cidr                = ["10.0.0.0/22"]
-      subnet_names        = ["SNET-PROD-SWT-WESTEU-OMS-FRONTEND", "SNET-PROD-SWT-WESTEU-OMS-BACKEND"],
+      subnet_names        = ["SNET-PROD-SWT-WESTEU-OMS-APP", "SNET-PROD-SWT-WESTEU-OMS-DATABASE"],
       snet_cidr           = ["10.0.0.0/23", "10.0.2.0/23"],
       resource_group_name = var.resource_group_name[0],
       nsg_name            = var.nsg_name[0],
@@ -10,7 +10,7 @@ locals {
     },
     "VNET-PROD-SWT-WESTEU-CMS" = {
       cidr                = ["10.1.0.0/22"]
-      subnet_names        = ["SNET-PROD-SWT-WESTEU-CMS-FRONTEND", "SNET-PROD-SWT-WESTEU-CMS-BACKEND"],
+      subnet_names        = ["SNET-PROD-SWT-WESTEU-CMS-APP", "SNET-PROD-SWT-WESTEU-CMS-DATABASE"],
       snet_cidr           = ["10.1.0.0/23", "10.1.2.0/23"],
       resource_group_name = var.resource_group_name[1]
       nsg_name            = var.nsg_name[1],
@@ -18,7 +18,7 @@ locals {
     },
     "VNET-PROD-SWT-WESTEU-PIMS" = {
       cidr                = ["10.2.0.0/22"]
-      subnet_names        = ["SNET-PROD-SWT-WESTEU-PIMS-FRONTEND", "SNET-PROD-SWT-WESTEU-PIMS-BACKEND"],
+      subnet_names        = ["SNET-PROD-SWT-WESTEU-PIMS-APP", "SNET-PROD-SWT-WESTEU-PIMS-DATABASE"],
       snet_cidr           = ["10.2.0.0/23", "10.2.2.0/23"],
       resource_group_name = var.resource_group_name[2],
       nsg_name            = var.nsg_name[2],
